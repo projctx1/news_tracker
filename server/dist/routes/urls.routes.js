43824be43d1bcbc8ecb5d0eef9a20efbc2ae572c
Your Name <you@example.com>
2025-08-25 04:17:24 +0100

    stop tracking dist build files

--- server/dist/routes/urls.routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _mongoose = _interopRequireDefault(require("mongoose"));
-var _express = _interopRequireDefault(require("express"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var urlsRoute = _express["default"].Router();
